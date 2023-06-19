04aed65b4f5c05fa8991c46647cce666b30b69d2
lawlist <esq@lawlist.com>
2023-06-18 21:27:39 -0700

    Added plain 12-month rotating calendar without marking events.

--- lorg-calendar.el
+++ lorg-calendar.el
@@ -1282,4 +1282,103 @@ Returns the list (month day year) giving the cursor position."
             (backward-char 1)))
         (lorg-calendar-cursor-to-date))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; https://stackoverflow.com/a/21409154/2112489
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;                                                                            ;;;
+;;; Scroll a yearly calendar by month -- in a forwards or backwards direction. ;;;
+;;;                                                                            ;;;
+;;; To try out this example, evaluate the entire code snippet and type:        ;;;
+;;;                                                                            ;;;
+;;;     M-x year-calendar                                                      ;;;
+;;;                                                                            ;;;
+;;; To scroll forward by month, type the key:  >                               ;;;
+;;;                                                                            ;;;
+;;; To scroll backward by month, type the key:  <                              ;;;
+;;;                                                                            ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defvar lawlist-twelve-month-mode-map
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map calendar-mode-map)
+    (define-key calendar-mode-map "<" 'lawlist-scroll-year-calendar-backward)
+    (define-key calendar-mode-map ">" 'lawlist-scroll-year-calendar-forward)
+    map)
+  "Local keymap for `tda-disabled-mode' buffers.")
+
+;;;###autoload
+(defun year-calendar (&optional month year)
+  "Generate a one (1) year calendar that can be scrolled by month in each direction.
+This is a modification of:  http://homepage3.nifty.com/oatu/emacs/calendar.html
+See also:  http://ivan.kanis.fr/caly.el"
+(interactive)
+  (require 'calendar)
+  (let* ((current-year (number-to-string (nth 5 (decode-time (current-time)))))
+         (month (if month month
+           (string-to-number
+             (read-string "Please enter a month number (e.g., 1):  " nil nil "1"))))
+         (year (if year year
+           (string-to-number
+             (read-string "Please enter a year (e.g., 2014):  "
+               nil nil current-year)))))
+    (switch-to-buffer (get-buffer-create calendar-buffer))
+    (when (not (eq major-mode 'calendar-mode))
+      (calendar-mode))
+    (use-local-map lawlist-twelve-month-mode-map)
+    (setq displayed-month month)
+    (setq displayed-year year)
+    (setq buffer-read-only nil)
+    (erase-buffer)
+    ;; horizontal rows
+    (lawlist-calendar-for-loop j from 0 to 3 do
+      ;; vertical columns
+      (lawlist-calendar-for-loop i from 0 to 2 do
+        (calendar-generate-month
+          ;; month
+          (cond
+            ((> (+ (* j 3) i month) 12)
+              (- (+ (* j 3) i month) 12))
+            (t
+              (+ (* j 3) i month)))
+          ;; year
+          (cond
+            ((> (+ (* j 3) i month) 12)
+             (+ year 1))
+            (t
+              year))
+          ;; indentation / spacing between months
+          (+ 5 (* 25 i))))
+      (goto-char (point-max))
+      (insert (make-string (- 10 (count-lines (point-min) (point-max))) ?\n))
+      (widen)
+      (goto-char (point-max))
+      (narrow-to-region (point-max) (point-max)))
+    (widen)
+    (goto-char (point-min))
+    (setq buffer-read-only t)))
+
+(defun lawlist-scroll-year-calendar-forward (&optional arg event)
+  "Scroll the yearly calendar by month in a forward direction."
+  (interactive (list (prefix-numeric-value current-prefix-arg)
+                     last-nonmenu-event))
+  (unless arg (setq arg 1))
+  (save-selected-window
+    (if (setq event (event-start event)) (select-window (posn-window event)))
+    (unless (zerop arg)
+      (let ((month displayed-month)
+            (year displayed-year))
+        (calendar-increment-month month year arg)
+        (year-calendar month year)))
+    (goto-char (point-min))
+    (run-hooks 'calendar-move-hook)))
+
+(defun lawlist-scroll-year-calendar-backward (&optional arg event)
+  "Scroll the yearly calendar by month in a backward direction."
+  (interactive (list (prefix-numeric-value current-prefix-arg)
+                     last-nonmenu-event))
+  (lawlist-scroll-year-calendar-forward (- (or arg 1)) event))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (provide 'lorg-calendar)
